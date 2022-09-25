7edc397f049625ac4e5345f3ddc51d86c495e376
Alexander <Alexander10100@yandex.ru>
2022-09-25 23:17:18 +0500

    Lab_3_v1

--- README.md
+++ README.md
@@ -2,16 +2,20 @@
 ____
 ### Автор программы: Жуков А. Ю.
 ____
-### Описание программы:
-Программа выводит на экран словами введенную сумму
-
-Следует открывать файл Lab_2.py из папки lab_2
-Открыть файл можно любой средой поддерживающей python
-
+### Лабораторная работа №3 - Шифр Цезаря:
+шифрование и расшифровка текста шифром Цезаря.
+____
+### Среда разработки
+Язык программирования python
+Среда разработки PyCharm
+____
+### Инструкция по работе
+Пользователь вводит строку и шаг сдвига.
+____
 ### Скриншоты выполнения программы:
-
-![Пример 1](https://github.com/Alexander10100/application-programming/tree/main/lab_2/Screenshots_program_execution/128.png)
-
-![Пример 2](https://github.com/Alexander10100/application-programming/tree/main/lab_2/Screenshots_program_execution/1003.png)
-
-![Пример 3](https://github.com/Alexander10100/application-programming/tree/main/lab_2/Screenshots_program_execution/256501.png)
+Пример 1
+![Пример 1](/lab_3/Screenshots_program_execution/1.png)
+Пример 2
+![Пример 2](/lab_3/Screenshots_program_execution/2.png)
+Пример 3
+![Пример 3](/lab_3/Screenshots_program_execution/3.png)
---
+++ lab_3/Фт-210007 Жуков А Ю.py
@@ -0,0 +1,60 @@
+def create_alphabet():
+    alphabet_low = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+    alphabet_high = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
+    return alphabet_low, alphabet_high
+
+
+def encryption(data, move, alphabet_low, alphabet_high):
+    result = ''
+    for i in data:
+        if i not in alphabet_low and i not in alphabet_high:
+            result += i
+        else:
+            count = alphabet_low.find(i.lower()) + move
+            while count < 0 or count > len(alphabet_low) - 1:
+                if count > len(alphabet_low) - 1:
+                    count -= len(alphabet_low)
+                elif count < 0:
+                    count += len(alphabet_low)
+            else:
+                if i in alphabet_low:
+                    result += alphabet_low[count]
+                else:
+                    result += alphabet_high[count]
+    key = move * -1
+    return result, key
+
+
+def main():
+    alphabet_low, alphabet_high = create_alphabet()
+    print(f'Изменяемые символы - {alphabet_low} / {alphabet_high}')
+    string = str(input('Введите строку, которую хотите зашифровать или расшифровать - '))
+    step = input('Введите числом шаг сдвига для шифровки или расшифровки - ')
+
+    try:
+        int(step)
+    except ValueError:
+        print('Неверно введён шаг сдвига')
+        return
+
+    key, result = int(step), string
+
+    if key / len(alphabet_low) == -1 or key / len(alphabet_low) == 1:
+        print('Предупреждение. Результат ничем не будет отличатся')
+
+    flag = 0
+    while flag == 0:
+        print('0 - шифровка/расшифровка\n'
+              '1 - выход')
+        operation = input('Введите номер нужной операций - ')
+        if operation == 0:
+            result, key = encryption(result, key, alphabet_low, alphabet_high)
+            print(f'Результат - {result}')
+            print('-------------------------------------------------')
+        elif operation == 1:
+            flag = 1
+        else:
+            print('Ошибка! Ведена несуществующая операция')
+
+
+main()
