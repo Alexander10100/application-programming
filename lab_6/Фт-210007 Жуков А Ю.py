98a9d4ce43bfcfa39bcd119734db191c6600d121
Alexander <Alexander10100@yandex.ru>
2022-10-15 22:47:21 +0500

    Добавление комментариев

--- lab_6/Фт-210007 Жуков А Ю.py
+++ lab_6/Фт-210007 Жуков А Ю.py
@@ -1,14 +1,14 @@
-def entering_values(string):
+def entering_values(string, limitation):  # Ввод значении программы
     number = ckeck(string)
-    if number < 1 or number > 9:
+    if number < 1 or number > limitation:
         print('Введены некорректные данные. Попробуйте снова')
-        return entering_values(string)
+        return entering_values(string, limitation)
     else:
         return number
 
 
-def creating_matrix(criteria):
-    matrix = [[0] * criteria for i in range(criteria)]
+def creating_matrix(criteria):  # Создание и заполнение матрицы
+    matrix = [[0] * criteria for i in range(criteria)]  # Создание 0 матрицы
     for i in range(criteria):
         for j in range(criteria):
             if i > j:
@@ -16,11 +16,11 @@ def creating_matrix(criteria):
             elif i == j:
                 matrix[i][j] = 1
             elif j > i:
-                matrix[i][j] = entering_values(f'Отношенее критерия {i+1} к {j+1} ')
+                matrix[i][j] = entering_values(f'Отношенее критерия {i+1} к {j+1} от 1 до 9: ', 9)
     return matrix
 
 
-def calculation_coefficients(amount, amount_line):
+def calculation_coefficients(amount, amount_line):  # Расчёт коэффициентов
     coef = list(map(lambda x: round(x / amount, 2), amount_line))
     if sum(coef) != 1:
         difference = 1 - sum(coef)
@@ -29,7 +29,7 @@ def calculation_coefficients(amount, amount_line):
     return coef
 
 
-def ckeck(string):
+def ckeck(string):  # Проверка на целое число
     n = input(string)
     try:
         n = int(n)
@@ -38,9 +38,9 @@ def ckeck(string):
     return n
 
 
-criteria = entering_values('Введите цифрой количество критериев: ')
-matrix = creating_matrix(criteria)
-amount_line = [sum(i) for i in matrix]
-amount = sum(amount_line)
-coefficients = calculation_coefficients(amount, amount_line)
-print(f'весовые коэффициенты: {coefficients}')
+criteria = entering_values('Введите цифрой количество критериев: ', 1000)
+matrix = creating_matrix(criteria)  # Создаем матрицу
+amount_line = [sum(i) for i in matrix]  # Считаем сумму строк
+amount = sum(amount_line)  # Подсчитываем общую сумму
+coefficients = calculation_coefficients(amount, amount_line)  # Получаем коэффициенты
+print(f'Весовые коэффициенты: {coefficients}')
