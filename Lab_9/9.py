454572729ce9a25e50814aef773b998ee5a89e86
Alexander <Alexander10100@yandex.ru>
2022-11-23 23:57:07 +0500

    Добавил создание лог файла

--- Lab_9/9.py
+++ Lab_9/9.py
@@ -5,6 +5,7 @@ import logging
 def more_less(guessed_number, number):
     if guessed_number > number:
         print('Загаданное число больше')
+        logging.info(f'Program printed Загаданное число больше')
     elif guessed_number < number:
         print('Загаданное число меньше')
 
@@ -12,18 +13,24 @@ def more_less(guessed_number, number):
 def guessing(guessed_number, attempts):
     for i in range(1, attempts + 1):
         print('Попытка №', i)
+        logging.info(f'Попытка № {i}')
+        logging.info(f'Program printed Введите число, которое, на ваш взгляд, загадал компьютер')
         number = ckeck('Введите число, которое, на ваш взгляд, загадал компьютер - ')
         if guessed_number == number:
             print('Вы отгадали!!! С попытки №', i)
+            logging.info(f'Program printed Вы отгадали!!! С попытки № {i}')
             return
         elif guessed_number != number:
             print('Неправильно. Осталось попыток:', attempts - i)
+            logging.info(f'Неправильно. Осталось попыток: {attempts - i}')
             if attempts - i != 0:
                 more_less(guessed_number, number)
         if i == 6:
             print('Сегодня не ваш день.')
+            logging.info(f'Program printed Сегодня не ваш день')
         print('')
     print('Вы не смогли отгадать число. Загаданное число -', guessed_number)
+    logging.info(f'Program printed Вы не смогли отгадать число. Загаданное число - {guessed_number}')
 
 
 def ckeck(string):
@@ -38,8 +45,17 @@ def ckeck(string):
     return int(n)
 
 
+logging.basicConfig(level=logging.DEBUG, filename="log.log", format="%(asctime)s %(levelname)s %(message)s")
+logging.info('Programm started')
 print('Компьютер загадывает целое число от 1 до N. Ваша цель угадать его.')
+logging.info(f'Program printed Компьютер загадывает целое число от 1 до N. Ваша цель угадать его.')
+logging.info(f'Program printed Введите N')
 N = ckeck('Введите N - ')
+logging.info(f'N = {N}')
+logging.info(f'Program printed Введите числом количество попыток')
 attempts = ckeck('Введите числом количество попыток - ')
+logging.info(f'attempts = {attempts}')
 guessed_number = randint(1, N)
+logging.info(f'guessed_number = {guessed_number}')
 guessing(guessed_number, attempts)
+logging.info('Programms end')
