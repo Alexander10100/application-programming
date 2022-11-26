29bd84ae3782400346546454c6ed21aa481e27ab
Alexander <Alexander10100@yandex.ru>
2022-11-26 12:12:12 +0500

    Добавил функцию decision

--- lab_8/Фт-210007 Жуков А Ю.py
+++ lab_8/Фт-210007 Жуков А Ю.py
@@ -10,23 +10,26 @@ def out_keg(list_num, result):
     return list_num, result
 
 
-N = int(input('Введите числом количество бочонков - '))
-result = []
-numbers = [i for i in range(1, N+1)]
-print(numbers)
-
-flag = 0
-while flag != 3:
-    print('1 - вытаскиваем один боченок\n'
-          '2 - вытаскиваем боченки пока они не закончатся\n'
-          '3 - прервать операции')
-    flag = int(input('Укажите номер нужной операции - '))
-    if flag == 1:
-        numbers, result = out_keg(numbers, result)
-    elif flag == 2:
-        for i in range(len(numbers)):
+def decision(numbers):
+    result = []
+    flag = 0
+    while flag != 3:
+        if len(numbers) == 0:
+             return result
+        print('1 - вытаскиваем один боченок\n'
+              '2 - вытаскиваем боченки пока они не закончатся\n'
+              '3 - прервать операции')
+        flag = int(input('Укажите номер нужной операции - '))
+        if flag == 1:
             numbers, result = out_keg(numbers, result)
-    print(result)
-    print(numbers)
-print(result)
+        elif flag == 2:
+            for i in range(len(numbers)):
+                numbers, result = out_keg(numbers, result)
+        else:
+            print('Такой операции нет!')
 
+
+N = int(input('Введите числом количество бочонков - '))
+numbers = [i for i in range(1, N+1)]
+result = decision(numbers)
+print(result)
