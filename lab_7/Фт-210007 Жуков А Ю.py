4ef819d5a9e67aa5a9c1cd0ea936ffd5bf866c7e
Alexander <Alexander10100@yandex.ru>
2022-11-26 11:50:13 +0500

    Логгер

--- lab_7/Фт-210007 Жуков А Ю.py
+++ lab_7/Фт-210007 Жуков А Ю.py
@@ -1,3 +1,6 @@
+import logging
+
+
 def beats_or_not(x2, y2, conditions):  # Бьёт фигура за один ход или нет
     flag = 1
     if conditions:
@@ -33,6 +36,7 @@ def entering_values(string, limitation):  # Ввод значении прогр
     number = ckeck(string)
     if number < 1 or number > limitation:
         print('Введены некорректные данные. Попробуйте снова')
+        logging.error('Incorrect number')
         return entering_values(string, limitation)
     else:
         return number
@@ -40,6 +44,7 @@ def entering_values(string, limitation):  # Ввод значении прогр
 
 def ckeck(string):  # Проверка на целое число
     n = input(string)
+    logging.info(f'Users input number {n}')
     try:
         n = int(n)
     except Exception:
@@ -47,7 +52,7 @@ def ckeck(string):  # Проверка на целое число
     return n
 
 
-def menace(x1, y1, x2, y2, fig, point1, point2):  # Может ли фигура с поля (x1, y1) попасть на поле (x2, y2) 
+def menace(x1, y1, x2, y2, fig, point1, point2):  # Может ли фигура с поля (x1, y1) попасть на поле (x2, y2)
     modul_diffe_x = abs(x1 - x2)
     modul_diffe_y = abs(y1 - y2)
     if fig == 1:
@@ -73,15 +78,25 @@ def menace(x1, y1, x2, y2, fig, point1, point2):  # Может ли фигура
             print(f"Для нападения передвиньте ферзя на поле ({x1};{y2})")
     else:
         print('Такой финуры нет')
+        logging.error('Incorrect number')
 
 
+logging.basicConfig(filename="logs.log", level=logging.INFO)
+logging.info("User started program")
 print('Координаты могут принимать значения от 1 до 8')
 x1 = entering_values("Введите x координату первого поля - ", 8)
+logging.info(f'x1 = {x1}')
 y1 = entering_values("Введите y координату первого поля - ", 8)
+logging.info(f'y1 = {y1}')
 x2 = entering_values("Введите x координату второго поля - ", 8)
+logging.info(f'x2 = {x2}')
 y2 = entering_values("Введите y координату второго поля - ", 8)
+logging.info(f'y2 = {y2}')
 
 print("Выберите фигуру: 1 - Конь, 2 - Слон, 3 - Ладья, 4 - Ферзь")
 fig = entering_values("Ваша фигруа - ", 4)
+logging.info(f'figure = {fig}')
+
 point1, point2 = colour(x1, y1, x2, y2)
 menace(x1, y1, x2, y2, fig, point1, point2)
+logging.info("Programm ended")
