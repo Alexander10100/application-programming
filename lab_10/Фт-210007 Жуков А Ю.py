0e9b34b2bbe58fd6629b06a428214ba90dc66383
Alexander <Alexander10100@yandex.ru>
2022-12-06 15:54:28 +0500

    Ограничение ввода

--- lab_10/Фт-210007 Жуков А Ю.py
+++ lab_10/Фт-210007 Жуков А Ю.py
@@ -3,9 +3,17 @@ def check(string):  # Проверка на целое число
     try:
         n = int(n)
     except Exception:
-        print('Введено не натуральное числоо. Попробуйте снова')
-        return check(string)
-    return int(n)
+        return 0
+    return n
+
+
+def entering_values(string, limitation):  # Ввод значении программы
+    number = check(string)
+    if number < 1 or number > limitation:
+        print('Введено не натуральное числоо или слишком большое. Попробуйте снова')
+        return entering_values(string, limitation)
+    else:
+        return number
 
 
 def subtraction(banknotes, N, i):
@@ -19,11 +27,11 @@ def subtraction(banknotes, N, i):
 
 banknotes_denominations = [64, 32, 16, 8, 4, 2, 1]
 quantity_banknotes = [0 for i in range(len(banknotes_denominations))]
-N = check('Натуральное число ')
+N = entering_values('Натуральное число, не больше чем 60000 - ', 63000)
 required_banknotes = list(zip(banknotes_denominations, quantity_banknotes))
 required_banknotes = [list(k) for k in required_banknotes]
 for i in range(len(required_banknotes)):
-     N, required_banknotes = subtraction(required_banknotes, N, i)
-print('Всего потребуется купюр ', sum([l[1] for l in required_banknotes]))
+    N, required_banknotes = subtraction(required_banknotes, N, i)
+print('Всего потребуется купюр', sum([l[1] for l in required_banknotes]))
 for j in required_banknotes:
     print(f'Купюр номиналом {j[0]} потребуется {j[1]}')
